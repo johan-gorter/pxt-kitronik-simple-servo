509dff5377ead29fb72e8f1a705d96aa2827a2e4
PhilAtKitronik <37149430+PhilAtKitronik@users.noreply.github.com>
2021-06-25 12:03:15 +0100

    initial commit

--- main.ts
+++ main.ts
@@ -1,1 +1,113 @@
+/**
+ * Blocks for driving the Kitronik Simple Servo Board
+ */
+//% weight=100 color=#00A654 icon="\uf013" block="Servo"
+//% groups='["180 Servo", "360 Servo"]'
 
+namespace kitronik_simple_servo {
+
+    /**
+     * Selection of the servo to drive
+     */
+    export enum ServoChoice {
+        //% block="1"
+        servo1,
+        //% block="2"
+        servo2,
+        //% block="3"
+        servo3
+    }
+
+    /**
+     * Selection of direction turn for continous mservos
+     */
+    export enum ServoDirection {
+        //% block="clockwise"
+        CW,
+        //% block="counter-clockwise"
+        CCW
+    }
+
+    /**
+     * gobal functions to set the servo pins for each servo 
+     */
+    let servo1Pin = AnalogPin.P8
+    let servo2Pin = AnalogPin.P15
+    let servo3Pin = AnalogPin.P16
+
+
+    /**
+     * Turn the selected servo to a required degree turn
+     */
+    //% groups="180 Servo"
+    //% blockId=kitronik_simple_servo_angle
+    //% block="set servo %servoSelection to angle %degrees"
+    //% color=#00A654
+    //% degrees.min=0 degrees.max=180
+    //% degrees.defl=90
+    //% degrees.fieldEditor="gridpicker"
+    //% weight=100 blockGap=8
+    export function setServoAngle(servoSelection: ServoChoice, degrees: number) {
+        if (servoSelection == ServoChoice.servo1){
+            pins.servoWritePin(servo1Pin, degrees);
+        }
+        else if (servoSelection == ServoChoice.servo2){
+            pins.servoWritePin(servo2Pin, degrees);
+        }
+        else if (servoSelection == ServoChoice.servo3){
+            pins.servoWritePin(servo3Pin, degrees);
+        }
+        
+    }
+
+    /**
+     * Set the selected servo back to neutral position
+     */
+    //% groups="180 Servo"
+    //% blockId=kitronik_simple_servo_neutral
+    //% block="set servo %servoSelection to neutral position"
+    //% color=#00A654
+    //% weight=100 blockGap=8
+    export function setServoNeutral(servoSelection: ServoChoice) {
+        if (servoSelection == ServoChoice.servo1){
+            pins.servoWritePin(servo1Pin, 90);
+        }
+        else if (servoSelection == ServoChoice.servo2){
+            pins.servoWritePin(servo2Pin, 90);
+        }
+        else if (servoSelection == ServoChoice.servo3){
+            pins.servoWritePin(servo3Pin, 90);
+        } 
+    }
+
+    /**
+     * Set the selected servo back to neutral position
+     */
+    //% groups="360 Servo"
+    //% blockId=kitronik_simple_servo_neutral
+    //% block="set servo %servoSelection to turn %direction at %percentSpeed"
+    //% percentSpeed.min=0 percentSpeed.max=100
+    //% percentSpeed.defl=50
+    //% color=#00A654
+    //% weight=100 blockGap=8
+    export function servoRunPercentage(servoSelection: ServoChoice, direction: ServoDirection, percentSpeed: number) {
+        let speed = 90
+        if (direction == ServoDirection.CW){
+            speed = Math.round(Math.map(percentSpeed, 0, 100, 90, 180))
+        }
+        else if (direction == ServoDirection.CCW){
+            speed = Math.round(Math.map(percentSpeed, 0, 100, 90, 0))
+        }
+
+        if (servoSelection == ServoChoice.servo1){
+            pins.servoWritePin(servo1Pin, speed);
+        }
+        else if (servoSelection == ServoChoice.servo2){
+            pins.servoWritePin(servo2Pin, speed);
+        }
+        else if (servoSelection == ServoChoice.servo3){
+            pins.servoWritePin(servo3Pin, speed);
+        } 
+    }
+
+}
