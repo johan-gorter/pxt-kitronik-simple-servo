08e4ba74b105fd9801ba9e234ffc67dc3f86a033
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-13 19:46:49 -0700

    initial code

---
+++ jacdac/main.ts
@@ -0,0 +1,56 @@
+//% deprecated
+namespace kitronik_simple_servo { }
+
+namespace modules {
+    /**
+     * Client for the servo1 servo
+     */
+    //% fixedInstance whenUsed block="kitronik servo1"
+    export const kitronikServo1 = new ServoClient("kitronik servo1?device=self")
+    /**
+     * Client for the servo2 servo
+     */
+    //% fixedInstance whenUsed block="kitronik servo2"
+    export const kitronikServo2 = new ServoClient("kitronik servo2?device=self")
+    /**
+     * Client for the servo3 servo
+     */
+    //% fixedInstance whenUsed block="kitronik servo3"
+    export const kitronikServo3 = new ServoClient("kitronik servo3?device=self")
+}
+
+namespace servers {
+    class ServoServer extends jacdac.Server {
+        readonly servoChoice: kitronik_simple_servo.ServoChoice
+        angle: number
+        enabled: boolean
+
+        constructor(name: string, servoChoice: kitronik_simple_servo.ServoChoice) {
+            super(jacdac.SRV_SERVO, { instanceName: name })
+            this.servoChoice = servoChoice
+            this.angle = 90
+            this.enabled = false
+            kitronik_simple_servo.servoStop(this.servoChoice)
+        }
+
+        handlePacket(pkt: jacdac.JDPacket) {
+            this.enabled = this.handleRegBool(pkt, jacdac.ServoReg.Enabled, this.enabled)
+            this.angle = this.handleRegValue(pkt, jacdac.ServoReg.Angle, jacdac.ServoRegPack.Angle, this.angle)
+
+            if (!this.enabled)
+                kitronik_simple_servo.servoStop(this.servoChoice)
+            else
+                kitronik_simple_servo.setServoAngle(this.servoChoice, this.angle)
+        }
+    }
+
+    function start() {
+        jacdac.productIdentifier = 0x3cc2d4b4
+        jacdac.startSelfServers(() => [
+            new ServoServer("servo1", kitronik_simple_servo.ServoChoice.servo1),
+            new ServoServer("servo2", kitronik_simple_servo.ServoChoice.servo2),
+            new ServoServer("servo3", kitronik_simple_servo.ServoChoice.servo3),
+        ])
+    }
+    start()
+}
