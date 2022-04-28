abd27cb6bdf3a9b859033a638c74521918952517
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-28 13:46:14 -0700

    Update jacdac/pxt.json, jacdac/test.ts

--- jacdac/test.ts
+++ jacdac/test.ts
@@ -1,17 +1,14 @@
 let angle = 90
 let da = 5
 forever(function() {
-    modules.kitronikServo1.setEnabled(true)
-    modules.kitronikServo2.setEnabled(true)
-    modules.kitronikServo3.setEnabled(true)
-    modules.kitronikServo1.setAngle(angle)
-    modules.kitronikServo2.setAngle(-angle)
-    modules.kitronikServo3.setAngle(angle / 2)
+    modules.kitronikServo1.setAngle(angle + 90)
+    modules.kitronikServo2.setAngle(-angle + 90)
+    modules.kitronikServo3.setAngle(angle / 2 + 45)
 
     if (angle > 90)
         da = -5
     else if (angle < -90)
         da = 5
     angle += da    
-    pause(50)
+    pause(100)
 })
