9fa3dad709e6005d7a373cb90f75ca9913a5b325
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-28 09:57:08 -0700

    Update jacdac/pxt.json, jacdac/main.ts

--- jacdac/main.ts
+++ jacdac/main.ts
@@ -17,49 +17,13 @@ namespace modules {
 }
 
 namespace servers {
-    class ServoServer extends jacdac.Server {
-        dpin: DigitalPin
-        apin: AnalogPin
-        angle: number
-        enabled: boolean
-        offset: number
-
-        constructor(name: string, dpin: DigitalPin, apin: AnalogPin) {
-            super(jacdac.SRV_SERVO, { instanceName: name })
-            this.dpin = dpin
-            this.apin = apin
-            this.angle = 90
-            this.offset = 0
-            this.enabled = false
-            this.sync()
-        }
-
-        handlePacket(pkt: jacdac.JDPacket) {
-            this.enabled = this.handleRegBool(pkt, jacdac.ServoReg.Enabled, this.enabled)
-            this.angle = this.handleRegValue(pkt, jacdac.ServoReg.Angle, jacdac.ServoRegPack.Angle, this.angle)
-            this.offset = this.handleRegValue(pkt, jacdac.ServoReg.Offset, jacdac.ServoRegPack.Offset, this.offset)
-            this.handleRegValue(pkt, jacdac.ServoReg.CurrentAngle, jacdac.ServoRegPack.CurrentAngle, this.angle + this.offset)
-
-            this.sync()
-        }
-
-        sync() {
-            if (!this.enabled)
-                pins.digitalWritePin(this.dpin, 0)
-            else {
-                const degrees = Math.clamp(0, 180, this.angle + this.offset)
-                pins.servoWritePin(this.apin, degrees)
-            }
-        }
-    }
-
     function start() {
         jacdac.productIdentifier = 0x3cc2d4b4
         jacdac.deviceDescription = "Kitronik Simple Servo"
         jacdac.startSelfServers(() => [
-            new ServoServer("servo1", DigitalPin.P8, AnalogPin.P8),
-            new ServoServer("servo2", DigitalPin.P15, AnalogPin.P15),
-            new ServoServer("servo3", DigitalPin.P16, AnalogPin.P16),
+            new jacdac.ServoServer(AnalogPin.P8, { instanceName: "S1" }),
+            new jacdac.ServoServer(AnalogPin.P15, { instanceName: "S2" }),
+            new jacdac.ServoServer(AnalogPin.P16, { instanceName: "S3" }),
         ])
     }
     start()
